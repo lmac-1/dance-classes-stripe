2e1c6b68d957903fe14b6ea68da2735eaa2507a0
lmac-1 <macartney.lucy@gmail.com>
2023-01-20 21:44:08 -0500

    responsive fix for partner class

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,7 +62,7 @@ export default function Home() {
           </div>
         )}
         {packageType === 2 && (
-          <div className="grid grid-cols-3 gap-3">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
             {prices.partner.map((price) => (
               <Package
                 key={price.hours}
